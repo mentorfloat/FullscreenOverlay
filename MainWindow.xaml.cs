9ccb58150a56b13fcc069026b763c08a787f4acc
Steven Mah <chunho01@hotmail.com>
2020-11-17 18:26:48 +1100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using GameOverlay.Windows;
+using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace FullscreenOverlay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static IntPtr gameclient;
+        private UtilityOverlay overlayinstance = new UtilityOverlay();
+        public static bool ShowOverlayText = false;
+        public static string myText = "Overlay is loaded!";
+        private static bool StopTimer = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TimerCheckWindows()
+        {
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 1)
+            };
+
+            timer.Tick += (sender, EventArgs) =>
+            {
+                if (StopTimer)
+                {
+                    timer.Stop();
+                }
+
+                if (StopTimer == false && WindowHelper.GetForegroundWindow() != overlayinstance.Myoverlaywin.Handle)
+                {
+                    WindowHelper.EnableBlurBehind(gameclient);
+                    overlayinstance.Myoverlaywin.Recreate();
+                }
+            };
+            timer.Start();
+        }
+
+        public IntPtr GetProcessHandle(string client)
+        {
+            IntPtr handle = IntPtr.Zero;
+            try
+            {
+                var processes = Process.GetProcessesByName(client);
+                if (processes.Length > 0)
+                {
+                    handle = processes[0].MainWindowHandle;
+                }
+                else
+                {
+                }
+            }
+            catch
+            {
+            }
+            return handle;
+        }
+
+        private void Button_On(object sender, RoutedEventArgs e)
+        {
+            gameclient = GetProcessHandle("League of Legends");
+
+            overlayinstance.CreateOverlay();
+            overlayinstance.Myoverlaywin.Create();
+
+            ShowOverlayText = true;
+            StopTimer = false;
+
+            overlayinstance.Myoverlaywin.Show();
+
+            TimerCheckWindows();
+        }
+
+        private void Button_Off(object sender, RoutedEventArgs e)
+        {
+            StopTimer = true;
+
+            overlayinstance.Myoverlaywin.Dispose();
+            overlayinstance.Mygfx.Dispose();
+        }
+    }
+}
---
+++ UtilityOverlay.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameOverlay.Drawing;
+using GameOverlay.Windows;
+
+namespace FullscreenOverlay
+{
+    class UtilityOverlay
+    {
+		public StickyWindow Myoverlaywin;
+		public Graphics Mygfx;
+
+		private Dictionary<string, SolidBrush> _brushes;
+		private Dictionary<string, Font> _fonts;
+		private Dictionary<string, Image> _images;
+
+		public void CreateOverlay()
+		{
+			_brushes = new Dictionary<string, SolidBrush>();
+			_fonts = new Dictionary<string, Font>();
+			_images = new Dictionary<string, Image>();
+
+			Mygfx = new Graphics()
+			{
+				MeasureFPS = false,
+				PerPrimitiveAntiAliasing = true,
+				TextAntiAliasing = true,
+				WindowHandle = IntPtr.Zero
+			};
+
+			Myoverlaywin = new StickyWindow(MainWindow.gameclient, Mygfx)
+			{
+				FPS = 30,
+				IsTopmost = true,
+				IsVisible = true,
+				AttachToClientArea = true
+			};
+
+			Myoverlaywin.DestroyGraphics += Window_DestroyGraphics;
+			Myoverlaywin.DrawGraphics += Window_DrawGraphics;
+			Myoverlaywin.SetupGraphics += Window_SetupGraphics;
+		}
+
+		private void Window_SetupGraphics(object sender, SetupGraphicsEventArgs e)
+		{
+			var gfx = e.Graphics;
+
+			if (e.RecreateResources)
+			{
+				foreach (var pair in _brushes) pair.Value.Dispose();
+				foreach (var pair in _images) pair.Value.Dispose();
+			}
+
+			_brushes["transparent"] = gfx.CreateSolidBrush(0, 0, 0, 0);
+			_brushes["redsemi"] = gfx.CreateSolidBrush(255, 0, 0, 160);
+			_brushes["orangesemi"] = gfx.CreateSolidBrush(253, 106, 2, 160);
+			_brushes["white"] = gfx.CreateSolidBrush(255, 255, 255);
+			_brushes["blacksemi"] = gfx.CreateSolidBrush(0, 0, 0, 100);
+
+			if (e.RecreateResources) return;
+
+			_fonts["corbel"] = gfx.CreateFont("Corbel", 16);
+
+		}
+
+		private void Window_DestroyGraphics(object sender, DestroyGraphicsEventArgs e)
+		{
+			foreach (var pair in _brushes) pair.Value.Dispose();
+			foreach (var pair in _fonts) pair.Value.Dispose();
+			foreach (var pair in _images) pair.Value.Dispose();
+		}
+
+		private void Window_DrawGraphics(object sender, DrawGraphicsEventArgs e)
+		{
+			var gfx = e.Graphics;
+			gfx.ClearScene();
+
+			DrawText(gfx);
+		}
+
+		private void DrawText(Graphics gfx)
+		{
+			if (MainWindow.ShowOverlayText)
+			{
+				var overlayText = new StringBuilder()
+					.Append(MainWindow.myText)
+					.ToString();
+				gfx.DrawTextWithBackground(_fonts["corbel"], _brushes["white"], _brushes["blacksemi"], 100, 100, overlayText);
+			}
+		}
+	}
+}
